115954608e6421e1c6b2d859ed3cf456bddf448f
Silvio Salazar <silviosalac@gmail.com>
2020-07-07 23:43:17 -0600

    [IngredientesPizza] Se crea la clase QuesoIngrediente y se define su precio en el metodo constructor

---
+++ PizzaPlaneta/Ingredientes/QuesoIngrediente.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlaneta.Pizza;
+
+namespace PizzaPlaneta.Ingredientes
+{
+	public class QuesoIngrediente : PizzaIngrediente
+	{
+		public QuesoIngrediente(IPizza pizza) : base(pizza, "queso")
+		{
+			this.precio = 0.3;
+		}
+	}
+}
