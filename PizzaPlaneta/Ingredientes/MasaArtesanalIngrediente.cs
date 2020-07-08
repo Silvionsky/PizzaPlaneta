154b893dd0843e8c98567706ed14805f62f36dc1
Silvio Salazar <silviosalac@gmail.com>
2020-07-07 23:08:10 -0600

    [MasaPizza] Se crea la clase MasaArtesanalIngrediente y se define su precio en el metodo constructor

---
+++ PizzaPlaneta/Ingredientes/MasaArtesanalIngrediente.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlaneta.Pizza;
+
+namespace PizzaPlaneta.Ingredientes
+{
+	public class MasaArtesanalIngrediente : PizzaIngrediente
+	{
+		public MasaArtesanalIngrediente(IPizza pizza) : base(pizza, "masaartesanal")
+		{
+			this.precio = 3.0;
+		}
+	}
+}
