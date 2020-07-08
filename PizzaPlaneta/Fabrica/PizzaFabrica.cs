7f83ffe58a51b3d6b2ba38a827bbe9ff2f086dd7
Silvio Salazar <silviosalac@gmail.com>
2020-07-08 01:22:09 -0600

    [IngredientesPizza] Se crea la clase PizzaFabrica para manejar las diferentes Pizzas a crear. Se establecen los tamanos de pizza y el ingrediente queso como opciones iniciales

---
+++ PizzaPlaneta/Fabrica/PizzaFabrica.cs
@@ -0,0 +1,28 @@
+﻿using PizzaPlaneta.Pizza;
+using PizzaPlaneta.Ingredientes;
+
+namespace PizzaPlaneta.Fabrica
+{
+	public static class PizzaFabrica
+	{
+		public static IPizza CrearPizza(IPizza pizza, string nombre)
+		{
+			switch (nombre)
+			{
+				case "pequena":
+
+				case "mediana":
+
+				case "grande":
+					return new PizzaBasica(nombre);
+					break;
+				case "queso":
+					return new QuesoIngrediente(pizza);
+					break;
+				default:
+					return null;
+					break;
+			}
+		}
+	}
+}
