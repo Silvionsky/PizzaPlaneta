e8780faa88e2edae7281fb919355e85389467b14
Silvio Salazar <silviosalac@gmail.com>
2020-07-08 02:29:23 -0600

    [Interfaz] Se agrega data binding de los inputs para el tamano de la pizza, la masa y el queso

--- PizzaPlaneta/Pages/CrearPizza.cshtml.cs
+++ PizzaPlaneta/Pages/CrearPizza.cshtml.cs
@@ -2,19 +2,38 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PizzaPlaneta.Pizza;
 using PizzaPlaneta.Fabrica;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace PizzaPlaneta.Pages
 {
 	public class CrearPizzaModel : PageModel
 	{	
-		public CrearPizzaModel()
+		[BindProperty] public string tamano { set; get; }
+
+		[BindProperty] public string masa { set; get; }
+
+		[BindProperty] public List<string> ingredientes { set; get; }
+
+		public ActionResult OnPost()
 		{
+			List<string> pedido = new List<string>();
+			pedido.Add(tamano);
+			pedido.Add(masa);
+			pedido.AddRange(ingredientes);
+
+			IPizza pizza = null;
+
+			foreach (string nombre in pedido)
+			{
+				pizza = PizzaFabrica.CrearPizza(pizza, nombre);
+			}
+
+			double costoTotal = pizza.GetPrecioTotal();
+			string ingredientesTotal = pizza.GetIngredientes();
 		
-		}
 
-		public void OnGet()
-		{
-
+			return Page();
 		}
 	}
 }
