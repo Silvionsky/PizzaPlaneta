f22a966d6238b199d4ae8098734cdf47b87ccde2
Silvio Salazar <silviosalac@gmail.com>
2020-07-08 05:25:00 -0600

    [IngredientesPizza] Se crea el ingrediente SalchichaIngrediente

---
+++ PizzaPlaneta/Ingredientes/SalchichaIngrediente.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlaneta.Pizza;
+
+namespace PizzaPlaneta.Ingredientes
+{
+	public class SalchichaIngrediente : PizzaIngrediente
+	{
+		public SalchichaIngrediente(IPizza pizza) : base(pizza, "Carne")
+		{
+			this.precio = 0.3;
+		}
+	}
+}
