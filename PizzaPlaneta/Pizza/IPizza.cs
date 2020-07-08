67e7702b294b45ca8455011494d829c90b4a9876
Silvio Salazar <silviosalac@gmail.com>
2020-07-07 22:25:48 -0600

    [TamanoPizza] La clase PizzaBasica ahora tiene 3 tamanos disponibles(pequena, mediana o grande). El tamano se define como parametro del metodo constructor

--- PizzaPlaneta/Pizza/IPizza.cs
+++ PizzaPlaneta/Pizza/IPizza.cs
@@ -4,6 +4,8 @@
 	{
 		string GetIngredientes();
 
+		double GetPrecioTotal();
+
 		double GetPrecio();
 	}
 }
---
+++ PizzaPlaneta/Pizza/PizzaBasica.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaPlaneta.Pizza
+{
+	public class PizzaBasica : IPizza
+	{
+		private string tamano;
+		
+		public PizzaBasica(string tamano)
+		{
+			this.tamano = tamano;
+		}
+
+		public string GetIngredientes()
+		{
+			return this.tamano;
+		}
+
+		public double GetPrecio()
+		{
+			double precio = 0.0;
+			switch (this.tamano)
+			{
+				case "pequena":
+					precio = 1.0;			
+					break;
+				case "mediana":
+					precio = 2.5;
+					break;
+				case "grande":
+					precio = 5.0;
+					break;
+			}
+
+			return precio;
+		}
+
+		public double GetPrecioTotal()
+		{
+			return GetPrecio();
+		}
+
+	}
+}
