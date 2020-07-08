7ffea19b6d687ab876c24bd6463b459c2c86ee18
Silvio Salazar <silviosalac@gmail.com>
2020-07-08 02:33:58 -0600

    [IngredientesPizza] Se crea la clase ingrediente HongosIngrediente y se define un precio en el constructor

---
+++ PizzaPlaneta/Ingredientes/HongosIngrediente.cs
@@ -0,0 +1,12 @@
+﻿using PizzaPlaneta.Pizza;
+
+namespace PizzaPlaneta.Ingredientes
+{
+	public class HongosIngrediente : PizzaIngrediente
+	{
+		public HongosIngrediente(IPizza pizza) : base(pizza, "hongos")
+		{
+			this.precio = 0.6;
+		}
+	}
+}
