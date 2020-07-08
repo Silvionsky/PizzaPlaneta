7861ab16b9aab23c138b554dcad215aea41ae1ef
Silvio Salazar <silviosalac@gmail.com>
2020-07-07 23:01:13 -0600

    [MasaPizza] Se crea la clase abstracta PizzaIngrediente. El tipo de masa puede variar bastante entonces se decide tratar como un ingrediente que hereda de esta nueva clase abstracta

---
+++ PizzaPlaneta/Ingredientes/PizzaIngrediente.cs
@@ -0,0 +1,31 @@
+﻿using PizzaPlaneta.Pizza;
+
+namespace PizzaPlaneta.Ingredientes
+{
+	public abstract class PizzaIngrediente : IPizza
+	{
+		private IPizza pizza;
+		private string nombre;
+		private double precio;
+
+		public PizzaIngrediente(IPizza pizza, string nombre)
+		{
+			this.pizza = pizza;
+			this.nombre = nombre;
+		}
+
+		public string GetIngredientes()
+		{
+			return pizza.GetIngredientes() + ", " + nombre;
+		}
+
+		public double GetPrecio()
+		{
+			return this.precio;
+		}
+		public double GetPrecioTotal()
+		{
+			return pizza.GetPrecioTotal() + GetPrecio();
+		}
+	}
+}
